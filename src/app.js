21a70570eee4f9ee71d77e2a58eac663acafbb87
catalina flores <cataflores85@gmail.com>
2025-08-09 18:01:03 -0400

    Generador-excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+function generateExcuses() {
+
+  //Arrays de excusas disponibles
+  let who = ["Mi perro", "Mi gato", "Mi hámster", "Mi pez dorado", "Mi loro"];
+  let action = ["comió", "rompió", "perdió", "destruyó", "aplastó"];
+  let what = ["mi tarea", "mi proyecto", "mis notas", "mi presentación", "mi portátil"];
+  let when = ["antes de clase", "anoche", "esta mañana", "ayer", "justo ahora"];
+
+  //Generar números aleatorios para seleccionar elementos de cada array de excusas
+  let whoNumber = Math.floor(Math.random() * who.length);
+  let actionNumber = Math.floor(Math.random() * action.length);
+  let whatNumber = Math.floor(Math.random() * what.length);
+  let whenNumber = Math.floor(Math.random() * when.length);
+
+  //Seleccionar un elemento de cada array de excusas en base a los números aleatorios generados
+  let whoWord = who[whoNumber];
+  let actionWord = action[actionNumber];
+  let whatWord = what[whatNumber];
+  let whenWord = when[whenNumber];
+
+  return whoWord + " " + actionWord + " " + whatWord + " " + whenWord;
+
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuses();
 };
